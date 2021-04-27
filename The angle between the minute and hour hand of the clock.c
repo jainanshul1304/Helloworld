65100a0728a7dab5ae620a8f528c816a4aefc5a5
jainanshul1304 <69392526+jainanshul1304@users.noreply.github.com>
2021-04-27 09:39:17 +0530

    Add files via upload

---
+++ The angle between the minute and hour hand of the clock.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+int main()
+{
+
+int hours;
+float minutes,a,b,t,c;
+
+//Taking input from the user i.e, hours and minutes
+printf("hours:");
+scanf("%d",&hours);
+
+
+printf("minutes:");
+scanf("%f",&minutes);
+
+//The minute input is to be divided by 5 in order to get where the minute hand is pointing at
+
+//Now there are two ways to calculate the difference between where the hour hand is pointin at and where the minute is pointing at  
+a = minutes / 5 + 12 - hours;
+
+b = hours - minutes / 5;
+/*Difference of angle between each consecutive time is 360 / 12 = 30 degrees
+
+For eg: Say the hour hand is pointing at 11 and the minute hand at 5 i.e,the time is 11:25 so the angle made should be 30 * 6 = 180 degrees 
+But as you'd notice it is slightly lesser than that
+Why because the clock hand has moved a bit towards 12
+So since 25 / 60 * 30 = 12.5(Difference of angle between consecutive time is 30 degrees)
+We'd have to subtract this from the 180 therfore 167.5
+
+
+*/
+t = 30 * a - minutes / 2; //minutes / 60 * 30 = minutes / 2
+
+c = 30 * b - minutes / 2;
+
+//Since we need the smaller angle which ever angle clock wise or anticlockwise is smaller should be printed
+if (t > c)
+{printf("%f degrees is the angle between the minute and the hour hand of the clock",c);
+}
+
+else
+{printf("%f degrees is the angle between the minute and the hour hand of the clock",t);
+}
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
