73cc842a7402fd59df76801cb557c9e92056739a
jainanshul1304 <69392526+jainanshul1304@users.noreply.github.com>
2021-07-26 14:04:40 +0530

    Update age_shoesize_trick.c

--- age_shoesize_trick.c
+++ age_shoesize_trick.c
@@ -10,12 +10,12 @@ printf("Press 1 to play the game & press any other key to leave\n");
 printf("\nDo you want to play this game:\n");
 scanf("%d",&Ans);
 
-printf("\nHas your birthday passed yet(Press 1 if yes Press 2 if no)\n");
-scanf("%d",&birthday);
-
 if(Ans == 1)
 {
 do{
+
+printf("\nHas your birthday passed yet(Press 1 if yes Press 2 if no)\n");
+scanf("%d",&birthday);
 
 printf("Mind-blowing math trick can tell your age and shoe size\n");
 
@@ -47,7 +47,7 @@ printf("\n %d \n",Result);}
 
 printf("\nVOILA, THE FIRST TWO NUMBERS OR FIRST NUMBER OF THE FOUR/THREE-DIGIT NUMBER YOU GET ARE YOUR SHOE SIZE, AND THE NEXT TWO NUMBERS ARE YOUR AGE\n");
 
-printf("\nNOTE:If your birthday has not passed yet then subtract 1!\n");
+
 
 
 printf("Do you want to play this game again? \n");
