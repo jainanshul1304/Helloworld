cfd3cb2b15bd32fcae812f99b432e100f1f554ce
jainanshul1304 <69392526+jainanshul1304@users.noreply.github.com>
2021-12-23 20:39:36 +0530

    Update Google Interview ques.c

--- Google Interview ques.c
+++ Google Interview ques.c
@@ -13,7 +13,7 @@ Since the standard base in c is 10 to change the base we are going to apply basi
 log (x) base 5 = log (x) base 10/log (5) base 10
 
 */
-    printf("\nIf the result obtained is an integer then it can be represented in the form of 5^n otherwise it cannot be represented in the form of 5^n\n");  
+    printf("\nIf the result obtained is an integer then it can be represented in the form of 5^n otherwise it cannot be represented in the form of 5^n\n");
     c = log(5);
     //Taking the input i.e,x from the user
     printf("The value of x is:");
@@ -28,14 +28,15 @@ log (x) base 5 = log (x) base 10/log (5) base 10
     n = t / c;
     //Finally printing value of n
     printf("%f = %f/%f\n", n, t, c);
-    
-    if(n == 0 || n == 1 || n == 2 || n == 3 || n == 4 || n == 5 || n == 6 || n == 7 || n == 8 || n == 9){
-    	printf("It can be represented in form of 5 raised to the power n");
-	}
-	else{
-		printf("It cannot be represented in the form of 5 raised to the power n");
-	}
-    
-    
+
+    if (n == 0 || n == 1 || n == 2 || n == 3 || n == 4 || n == 5 || n == 6 || n == 7 || n == 8 || n == 9)
+    {
+        printf("It can be represented in form of 5 raised to the power n");
+    }
+    else
+    {
+        printf("It cannot be represented in the form of 5 raised to the power n");
+    }
+
     return 0;
 }
