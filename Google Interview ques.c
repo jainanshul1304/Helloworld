dba64b07667c40261d89f71f42023181face3bcf
jainanshul1304 <69392526+jainanshul1304@users.noreply.github.com>
2021-12-23 20:38:10 +0530

    Update Google Interview ques.c

--- Google Interview ques.c
+++ Google Interview ques.c
@@ -1,8 +1,5 @@
-//A C Program to determine whether a no can be represented in the form of 5^n(5 raised to the power n)
-
 #include <stdio.h>
 #include <math.h>
-#include <conio.h>
 #include <string.h>
 
 int main()
@@ -16,10 +13,10 @@ Since the standard base in c is 10 to change the base we are going to apply basi
 log (x) base 5 = log (x) base 10/log (5) base 10
 
 */
-
+    printf("\nIf the result obtained is an integer then it can be represented in the form of 5^n otherwise it cannot be represented in the form of 5^n\n");  
     c = log(5);
     //Taking the input i.e,x from the user
-    printf("the value of x is:");
+    printf("The value of x is:");
     scanf("%f", &x);
 
     t = log(x);
@@ -31,7 +28,14 @@ log (x) base 5 = log (x) base 10/log (5) base 10
     n = t / c;
     //Finally printing value of n
     printf("%f = %f/%f\n", n, t, c);
-    scanf("%f%f%f", &t, &c, &n);
-
+    
+    if(n == 0 || n == 1 || n == 2 || n == 3 || n == 4 || n == 5 || n == 6 || n == 7 || n == 8 || n == 9){
+    	printf("It can be represented in form of 5 raised to the power n");
+	}
+	else{
+		printf("It cannot be represented in the form of 5 raised to the power n");
+	}
+    
+    
     return 0;
 }
